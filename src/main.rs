9d46e70a4eb1f218a002b60a59ddee2d5f6cb311
btc <734339+btc@users.noreply.github.com>
2021-07-29 14:17:10 -0400

    init

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+/*
+ * TODO: get default-branch name
+ * TODO: import CLI framework
+ * TODO: create CLI commands
+ * TODO: import tokio
+ * TODO: grasp
+ * TODO: create new branch
+ * TODO: create new PR
+ * TODO: access GitHub API in Rust
+ * TODO: update PR chain
+ * TODO: rebase PR on main if dependent PR is merged
+ */
+fn main() {
+    println!("Hello, world!");
+}
