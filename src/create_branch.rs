639b7cfd983dcb091732a698a2ff03b416f0a741
btc <734339+btc@users.noreply.github.com>
2021-07-31 13:19:25 -0400

    parse seq no

--- src/create_branch.rs
+++ src/create_branch.rs
@@ -1,5 +1,6 @@
 use anyhow::anyhow;
 use anyhow::Context;
+use anyhow::Result;
 use git2::BranchType::Local;
 use git2::Repository;
 
@@ -17,4 +18,36 @@ pub fn create_branch(repo: &Repository) -> anyhow::Result<String> {
     // parse pattern
     // define tests
     Err(anyhow!("TODO"))
+}
+
+fn parse_sequence_number<S: AsRef<str>>(branch_name: S) -> Result<i32> {
+    let s = branch_name.as_ref()
+        .strip_prefix("btc/")
+        .ok_or(anyhow!("name doesn't begin with prefix"))?
+        .trim_start_matches("0")
+        .splitn(2, "-").take(1)
+        .collect::<String>();
+    Ok(s.parse::<i32>()?.into())
+}
+
+fn next_number_in_sequence(last: i32) -> i32 {
+    last + 1 // TODO compute primes
+}
+
+mod test {
+    use crate::create_branch::*;
+
+    #[test]
+    fn test_parse_sequence_number() {
+        let cases = vec![
+            ("btc/1000", Some(1000)),
+            ("btc/0041", Some(41)),
+            ("btc/1001", Some(1001)),
+            ("main", None),
+            ("btc/0001-foobar", Some(1))];
+        for (name, expected) in cases {
+            let got = parse_sequence_number(name);
+            assert_eq!(expected, got.ok());
+        }
+    }
 }
