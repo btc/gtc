753103f05fb9cc8aaf7c827721c87d2a12c7f975
btc <734339+btc@users.noreply.github.com>
2021-08-01 11:18:18 -0400

    wip

--- src/grasp.rs
+++ src/grasp.rs
@@ -1,13 +1,26 @@
-use git2::Repository;
+use anyhow::anyhow;
+use anyhow::Result;
+use git2::RepositoryState::Clean;
+use git2::{Repository, StatusOptions};
 
-pub fn grasp(repo: Repository) -> anyhow::Result<()> {
+pub fn grasp(repo: Repository) -> Result<()> {
+    if !is_clean(repo)? {
+        return Err(anyhow!("repo must be clean"));
+    }
     /* TODO
-    ensure repo is clean
     checkout default
     fetch
     rebase default on origin/default
     checkout -
     rebase on default
      */
-    todo!()
+    Ok(())
+}
+
+fn is_clean(repo: Repository) -> Result<bool> {
+    let mut opts = StatusOptions::new();
+    opts.include_ignored(false);
+    let statuses = repo.statuses(Some(&mut opts))?;
+    let is_clean = statuses.iter().len() == 0;
+    Ok(is_clean)
 }
