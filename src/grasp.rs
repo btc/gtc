eef902b7b4a8bb82b793c632ee91adab25aa727c
btc <734339+btc@users.noreply.github.com>
2021-08-01 11:42:50 -0400

    wip

--- src/grasp.rs
+++ src/grasp.rs
@@ -1,11 +1,17 @@
 use anyhow::anyhow;
 use anyhow::Result;
 use git2::{Repository, StatusOptions};
+use crate::default_branch_name::default_branch_name;
 
 pub fn grasp(repo: Repository) -> Result<()> {
-    if !is_clean(repo)? {
+
+    if !is_clean(&repo)? {
         return Err(anyhow!("repo must be clean"));
     }
+
+    let default = default_branch_name(&repo)?;
+    switch(&repo, &default)?;
+
     /* TODO
     checkout default
     fetch
@@ -16,7 +22,17 @@ pub fn grasp(repo: Repository) -> Result<()> {
     Ok(())
 }
 
-fn is_clean(repo: Repository) -> Result<bool> {
+fn switch(repo: &Repository, branch: &str) -> Result<()> {
+    let reference = repo
+        .resolve_reference_from_short_name(branch)?
+        .name()
+        .ok_or(anyhow!("failed to resolve reference"))?
+        .to_string();
+    repo.set_head(&reference)?;
+    Ok(())
+}
+
+fn is_clean(repo: &Repository) -> Result<bool> {
     let mut opts = StatusOptions::new();
     opts.include_ignored(false);
     let statuses = repo.statuses(Some(&mut opts))?;
